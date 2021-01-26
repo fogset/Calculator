89544710b54c5d6bfb597aab47034f9382e49682
fogset <zhangth1990@hotmail.com>
2021-01-25 19:09:45 -0800

    start to use bodyparser

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+app.user(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + "/index.html");
@@ -15,5 +18,5 @@ app.get('/hobbies', (request, response) => {
 });
 
 app.post("/", function(req, res){
-  res.send("Thanks for posting that!")
+  res.send("Thanks for posting that!");
 })
