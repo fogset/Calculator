a909753ef690d36554e3c74a46fb1ff3d57371ee
fogset <zhangth1990@hotmail.com>
2021-01-25 23:38:20 -0800

    fix error for bmi calculator

--- calculator.js
+++ calculator.js
@@ -3,7 +3,9 @@ const port = 3000;
 const bodyParser = require('body-parser')
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + "/index.html");
@@ -17,16 +19,16 @@ app.get('/bmicalculator', (request, response) => {
   response.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
   res.send("the result of the calculation is " + result);
 });
 
-app.post("/bmicalculator", function(req, res){
-  var weight = Number(req.body.weight);
-  var height = Number(req.body.height);
-  var result = weight * height;
-  res.send("Your BMI is " + result);
+app.post("/bmicalculator", function(req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+  res.send("Your BMI is " + bmi);
 });
